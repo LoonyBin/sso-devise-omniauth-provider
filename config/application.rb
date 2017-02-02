bff6230603e774f2a2b4e0794dfef5cf0bcbdba4
Subhash Chandra <TMaYaD@LoonyB.in>
2017-02-02 10:31:30 +0530

    Prep for deployment

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module CfiOauthProvider
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = 'utf-8'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
@@ -47,5 +47,77 @@ module CfiOauthProvider
 
     # Mass assingment protection
     config.active_record.whitelist_attributes=true
+
+    # The site name that will be used in text such as "Sign in to site_title"
+    config.site_title = ENV.fetch 'SITE_TITLE', 'Arvados'
+
+    # After logging in, the title and URL of the link that will be presented to
+    # the user as the default destination on the welcome page.
+    config.default_link_title = ENV.fetch 'LINK_TITLE', config.site_title
+    config.default_link_url = ENV.fetch 'LINK_URL', 'http://localhost:3000'
+
+    # secret_token is a string of alphanumeric characters used by Rails
+    # to sign session tokens. IMPORTANT: This is a site secret. It
+    # should be at least 50 characters.
+    config.secret_token = ENV['SECRET_TOKEN']
+
+    # The uuid prefix for generating new Arvados user uuids.  Must be exactly
+    # five characters long in the range [a-z0-9].
+    config.uuid_prefix = ENV['UUID_PREFIX']
+
+    # You really want to run everything over SSL.
+    config.force_ssl = true
+
+    ###
+    ### Local account configuration.  This is enabled if neither
+    ### google_oauth2 or LDAP are enabled below.
+    ###
+    # If true, allow new creation of new accounts in the SSO server's internal
+    # user database.
+    config.allow_account_registration = false
+
+    # If true, send an email confirmation before activating new accounts in the
+    # SSO server's internal user database.
+    config.require_email_confirmation = false
+
+
+    ###
+    ### Google+ OAuth2 authentication.
+    ###
+    # Google API tokens required for OAuth2 login.
+    #
+    # See https://github.com/zquestz/omniauth-google-oauth2
+    #
+    # and https://developers.google.com/accounts/docs/OAuth2
+    config.google_oauth2_client_id = false
+    config.google_oauth2_client_secret = false
+
+    # Set this to your OpenId 2.0 realm to enable migration from Google OpenId
+    # 2.0 to Google OAuth2 OpenId Connect (Google will provide OpenId 2.0 user
+    # identifiers via the openid.realm parameter in the OAuth2 flow until 2017).
+    config.google_openid_realm = false
+
+    # Enable login via Google OpenId 2.0 (note this is scheduled to be shut off 20 April 2015!)
+    config.google_deprecated_openid = false
+
+
+    ###
+    ### LDAP authentication.
+    ###
+    #
+    # If you want to use LDAP, you need to provide
+    # the following set of fields under the use_ldap key.
+    #
+    # use_ldap: false
+    #   title: Example LDAP
+    #   host: ldap.example.com
+    #   port: 636
+    #   method: ssl
+    #   base: "ou=Users, dc=example, dc=com"
+    #   uid: uid
+    #   email_domain: example.com
+    #   #bind_dn: "some_user"
+    #   #password: "some_password"
+    config.use_ldap = false
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,3 @@
-require_relative 'load_config.rb'
-
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
